7b36bc6c29bfa0a1b18b7e21a53d0c1dbc064491
hbauhvagner <hbauhvagner@gmail.com>
2023-07-11 23:03:22 -0300

    Requisição POST

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,6 +5,26 @@ async function listaVideos() {
     return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = fetch('http://localhost:3000/videos', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            titulo,
+            descricao: `${descricao} mil visualizações`,
+            url,
+            imagem
+        })
+    });
+
+    const conexaoConvertida = (await conexao).json();
+
+    return conexaoConvertida;
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
